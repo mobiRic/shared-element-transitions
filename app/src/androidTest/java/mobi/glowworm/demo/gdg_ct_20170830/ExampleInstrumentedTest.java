6faaccfda4934b4edf0a875ddea911c7051be726
Richard Le Mesurier <richard@glowworm.mobi>
2017-08-28 12:39:42 +0200

    initial Master/Detail sample app

---
+++ app/src/androidTest/java/mobi/glowworm/demo/gdg_ct_20170830/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package mobi.glowworm.demo.gdg_ct_20170830;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("mobi.glowworm.demo.gdg_ct_20170830", appContext.getPackageName());
+    }
+}
